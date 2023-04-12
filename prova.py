ee80cf8d216e55fc9527b7314a17693fd3bb6dd3
gulbord <52690645+gulbord@users.noreply.github.com>
2023-04-12 17:36:02 +0200

    Try formatting

--- prova.py
+++ prova.py
@@ -1,10 +1,12 @@
 # Sum of natural numbers up to num
 
-num =16
+num = 16
 
-if num < 0:print("Enter a positive number")
-else:sum = 0
-# use while loop to iterate until zero
+if num < 0:
+    print("Enter a positive number")
+else:
+    sum = 0
+    # use while loop to iterate until zero
     while num > 0:
         sum += num
         num -= 1
