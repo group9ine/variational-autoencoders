f80df6c6f5a1912aa4cee02c990f0596288e8793
Davide <bacilieri.davide@gmail.com>
2023-05-04 14:19:44 +0200

    added regularization l (tuneable), changed layers

--- vae.py
+++ vae.py
@@ -19,23 +19,21 @@ class Sampling(layers.Layer):
 latent_dim = 2
 l=0
 
-encoder_inputs = keras.Input(shape=(28, 28, 1))
-x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Flatten()(x)
-x = layers.Dense(16, activation="relu")(x)
+encoder_inputs = keras.Input(shape=(60,))
+x = layers.Dense(45, activation="relu")(encoder_inputs)
+x = layers.Dense(30, activation="relu")(x)
+x = layers.Dense(15, activation="relu")(x)
 z_mean = layers.Dense(latent_dim, name="z_mean")(x)
 z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
 z = Sampling()([z_mean, z_log_var])
 encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-encoder.summary()
+#encoder.summary()
 
 latent_inputs = keras.Input(shape=(latent_dim,))
-x = layers.Dense(7 * 7 * 64, activation="relu")(latent_inputs)
-x = layers.Reshape((7, 7, 64))(x)
-x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+x = layers.Dense(15, activation="relu")(latent_inputs)
+x = layers.Dense(30, activation="relu")(x)
+x = layers.Dense(45, activation="relu")(x)
+decoder_outputs = layers.Dense(60, activation="sigmoid")(x)
 decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
 decoder.summary()
 
