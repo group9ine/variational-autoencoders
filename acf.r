f0c310cfca4b322c10eb95f262cf01bd61713287
Davide <bacilieri.davide@gmail.com>
2023-05-09 11:53:32 +0200

    manual R version for autocorrelationn function

---
+++ acf.r
@@ -0,0 +1,16 @@
+acf <- function(data, h) {
+ sum(data[(h+1):length(data[,1]),]*data[1:(length(data[,1])-h),])/(90*(length(data[,1])-h))
+}
+
+get_correls <- function(data, step=100, stop=NA) {
+    correls <- c()
+    if(is.na(stop)){
+        stop <- (length(data[,1])%/%step -1)
+    }
+    for(i in 0:stop) {
+        a <- acf(data, i*step)
+        correls <- c(correls,a)
+    #     print(c(i*100, a))
+    }
+    correls
+}
