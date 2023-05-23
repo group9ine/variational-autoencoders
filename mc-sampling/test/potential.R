dc2bcbb1e1d75285a86a49a43b6d029cef32698c
gulbord <52690645+gulbord@users.noreply.github.com>
2023-05-23 17:22:25 +0200

    File for filming

--- mc-sampling/test/potential.R
+++ mc-sampling/test/potential.R
@@ -3,14 +3,14 @@ library(ggplot2)
 theme_set(theme_minimal(base_size = 16))
 pal <- RColorBrewer::brewer.pal(3, "Dark2")
 
-niter <- 10000
 fname <- list.files(
     path = "../dump", pattern = "*_U.txt", full.names = TRUE
 )
-potential <- scan(file = fname, sep = "\n")
 
-tibble(iter = seq_along(potential)[-1], pot = potential[-1]) |>
+potential <- scan(file = fname) |> matrix(nrow = 10000, ncol = 100)
+
+pot_plt <- potential[, 50]
+tibble(iter = seq_along(pot_plt), pot = pot_plt) |>
     ggplot(aes(x = iter, y = pot)) +
         geom_point(size = 0.6, colour = "gray40", alpha = 0.5) +
-        geom_smooth(colour = pal[1], fill = pal[1]) +
         labs(x = "Iteration", y = "Potential")
