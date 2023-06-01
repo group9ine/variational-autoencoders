bacac82cd58725955836c157004cd79f58a338d4
gulbord <52690645+gulbord@users.noreply.github.com>
2023-06-01 19:47:57 +0200

    Test run

--- mc-sampling/test/potential.R
+++ mc-sampling/test/potential.R
@@ -3,14 +3,11 @@ library(ggplot2)
 theme_set(theme_minimal(base_size = 16))
 pal <- RColorBrewer::brewer.pal(3, "Dark2")
 
-fname <- list.files(
-    path = "../dump", pattern = "*_U.txt", full.names = TRUE
-)
+fname <- "../dump/gamma_0.1_30_10_0.75_0.2_1_100000_10_U.txt"
 
-potential <- scan(file = fname) |> matrix(nrow = 10000, ncol = 100)
+potential <- scan(file = fname)
 
-pot_plt <- potential[, 20]
-tibble(iter = seq_along(pot_plt), pot = pot_plt) |>
+tibble(iter = seq_along(potential), pot = potential) |>
     ggplot(aes(x = iter, y = pot)) +
         geom_point(size = 0.6, colour = "gray40", alpha = 0.5) +
         labs(x = "Iteration", y = "Potential")
