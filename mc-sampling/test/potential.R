b1dc1ebb816b2fdf680278d6d45ec4094916a471
gulbord <52690645+gulbord@users.noreply.github.com>
2023-06-12 11:55:13 +0200

    Add potential plot for slides

--- mc-sampling/test/potential.R
+++ mc-sampling/test/potential.R
@@ -1,16 +1,28 @@
 library(tibble)
 library(ggplot2)
-theme_set(theme_minimal(base_size = 16))
+theme_set(theme_minimal(
+    base_size = 20, base_family = "Fira Sans Condensed"
+))
 pal <- RColorBrewer::brewer.pal(3, "Dark2")
 
 fname <- list.files(
     path = "../dump", pattern = "*_U.txt", full.names = TRUE
 )
 
-potential <- scan(file = fname) |> matrix(nrow = 10000, ncol = 100)
+potential <- scan(file = fname) |>
+    matrix(nrow = 1000, ncol = 500, byrow = TRUE)
 
-pot_plt <- potential[, 20]
-tibble(iter = seq_along(pot_plt), pot = pot_plt) |>
+idx <- 70
+tibble(iter = seq_along(potential[idx, ]), pot = potential[idx, ]) |>
     ggplot(aes(x = iter, y = pot)) +
-        geom_point(size = 0.6, colour = "gray40", alpha = 0.5) +
-        labs(x = "Iteration", y = "Potential")
+        geom_point(size = 1, colour = "firebrick") +
+        scale_x_continuous(labels = \(x) x * 10) +
+        labs(
+            x = "Iteration", y = "Potential",
+            title = "Evolution with γ = 0.7, T = 0.75"
+        )
+
+ggsave(
+    filename = "decorrelation.png", device = "png",
+    dpi = 600, width = 9, height = 5
+)
